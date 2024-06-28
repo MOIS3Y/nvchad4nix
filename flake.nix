33898ab8b4d915d0828f5b3431b44a052d701594
MOIS3Y <stepan@zhukovsky.me>
2024-06-28 23:44:19 +0900

    Update: remove flake-utils, now use custom build func for allDefaultSystems

--- flake.nix
+++ flake.nix
@@ -15,23 +15,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      {
-        packages = rec {
-          nvchad = pkgs.callPackage ./nix/nvchad.nix {};
-          default = nvchad;
-        };
-        overlays = rec {
-          nvchad = import ./nix/overlays.nix {};
-          default = nvchad;
-        };
-        homeManagerModules.nvchad = import ./nix/module.nix;
-        homeManagerModule = self.homeManagerModules.nvchad;
+  outputs = { self, nixpkgs }: 
+  let
+    lib = nixpkgs.lib;
+    genSystems = lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    pkgsFor = genSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.nvchad ];
       }
     );
+  in {
+    packages = genSystems (system:
+      (self.overlays.default pkgsFor.${system} pkgsFor.${system})
+      // { default = self.packages.${system}.nvchad; }
+    );
+    overlays = (import ./nix/overlays.nix { })
+    // { default = self.overlays.nvchad ; };
+    homeManagerModules.nvchad = import ./nix/module.nix;
+    homeManagerModule = self.homeManagerModules.nvchad;
+  };
 }
